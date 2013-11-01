023b05e501d99fb447dab935a5d15bc03186cb66
Edward Look <edwlook@gmail.com>
2013-11-01 11:55:44 -0700

    Use angular.forEach

--- app/assets/javascripts/angular/controllers/widgets/videoController.js
+++ app/assets/javascripts/angular/controllers/widgets/videoController.js
@@ -13,16 +13,16 @@
   		var sort = function(obj) {
   			var title = obj.mediapackage.title;
   			var tracks = obj.mediapackage.media.track;
-  			tracks.forEach(function(data) {
-  				if (data.mimetype === 'video/mp4') {
-  					$scope.videos.push({
-  						title: title,
-  						link: data.url
-  					});
-  				}
-  			});
-  		}
-  		$scope.data.forEach(sort);
+        angular.forEach(tracks, function(track) {
+          if (track.mimetype === 'video/mp4') {
+           $scope.videos.push({
+             title: title,
+             link: track.url
+           });
+          };
+        });
+  		};
+      angular.forEach($scope.data, sort)
   	};
 
   	var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22http%3A%2F%2Fplayback-qa.ets.berkeley.edu%2Fsearch%2FpaellaEpisodeListing.json%3Fq%3D%26sid%3D0570faa9-c983-42a2-a788-95c11ae06cca%26limit%3D100000%26offset%3D0%26_%3D1383056817297%22&format=json&callback=JSON_CALLBACK';
