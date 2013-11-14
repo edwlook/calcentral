e9d697ad3e3caa11668c32d364305e0cb198aa00
Edward Look <edwlook@gmail.com>
2013-11-14 12:19:00 -0800

    Data-setup bug fixed on partial reload

--- app/assets/javascripts/angular/controllers/widgets/videoController.js
+++ app/assets/javascripts/angular/controllers/widgets/videoController.js
@@ -4,29 +4,39 @@
   /**
    * Video controller
    */
-  calcentral.controller('VideoController', ['$http', '$scope', function($http, $scope) {
+  calcentral.controller('VideoController', ['$http', '$scope', '$timeout', function($http, $scope, $timeout) {
 
     $scope.videos = [];
     $scope.data;
-    var ccns = [];
+
+    var initPlayer = function() {
+      $timeout(function() {
+        var elements = document.getElementsByTagName('video');
+        for (var i = 0; i < elements.length; i++) {
+          _V_(elements[i], { "controls": true, "autoplay": false, "preload": "none" });
+        }
+      }, 1)
+    };
 
     // Execute request via /api/my/videos/:ccns as a comma separated list of CCNs
     // Add any videos retrieved to $scope.videos
-    var requestVideos = function() {
+    var requestVideos = function(ccns) {
       $http.get('/api/my/videos/' + ccns.join(',')).success(function(data) {
-        $scope.videos = data.videos;
+        angular.extend($scope.videos, data.videos);
+        initPlayer();
       });
     };
 
     // Collect class page CCNs and execute request once available
     $scope.$watchCollection('[$parent.selected_course.sections, $parent.selected_semester]', function(newValues) {
       if (newValues[0] && newValues[1]) {
-        angular.forEach(newValues[0], function(section) {
-          ccns.push(section.ccn);
-        });
         $scope.year = newValues[1].year,
         $scope.semesterCode = newValues[1].code;
-        requestVideos();
+        var ccns = [];
+        angular.forEach(newValues[0], function(section) {
+          ccns.push($scope.year + $scope.semesterCode + section.ccn);
+        });
+        requestVideos(ccns);
       }
     });
 
