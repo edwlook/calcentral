b0b5b6aeda330a399c8a4f510ed534ce0ad0e9b4
Edward Look <edwlook@gmail.com>
2013-12-03 14:48:48 -0800

    Only make one API call to get videos

--- app/assets/javascripts/angular/controllers/widgets/videoController.js
+++ app/assets/javascripts/angular/controllers/widgets/videoController.js
@@ -4,62 +4,29 @@
   /**
    * Video controller
    */
-  calcentral.controller('VideoController', ['$http', '$scope', '$timeout', function($http, $scope, $timeout) {
+  calcentral.controller('VideoController', ['$http', '$scope', function($http, $scope) {
 
-    $scope.videos = [];
-    $scope.data;
+    $scope.errorMessage = 'There are no videos.';
 
-    var getVideoLink = function(playlistID) {
-      $http.get('/api/my/videos_youtube/' + playlistID).success(function(data) {
+    var getVideos = function(courseDepartment, courseCategory, courseSection, courseSemester) {
+      var title = courseDepartment + ' ' + courseCategory + ', ' + courseSection + ' - ' + courseSemester;
+      $http.get('/api/my/videos/' + title).success(function(data) {
         angular.extend($scope, data);
-        console.log($scope.videos);
-      })
-    };
-
-    var getVideoPlaylist = function(courseTitle, courseSection, courseSemester) {
-      var playlistID = '';
-      var testTitle = courseTitle + ', ' + '001' + ' - ' + courseSemester;
-      $http.get('/json/videos.json').success(function(data) {
-        angular.forEach(data.courses, function(course) {
-          if (course.semester == courseSemester && course.title.toLowerCase() == testTitle.toLowerCase()) {
-            playlistID = course.youTube;
-            return
-          }
-        })
-        getVideoLink(playlistID);
       })
     };
 
     var formatClassTitles = function() {
-      // console.log($scope.selected_course);
-      var courseTitle = $scope.selected_course.course_number;
+      //console.log($scope.selected_course);
+      var courseDepartment = $scope.selected_course.dept_desc;
+      var courseCategory = $scope.selected_course.course_catalog;
       var courseSection = $scope.selected_course.sections[0].section_number;
       var courseSemester = $scope.selected_semester.name
-      // var title = courseTitle.toLowerCase();
-      // console.log(title);
-      getVideoPlaylist(courseTitle, courseSection, courseSemester);
-    }
-    // Execute request via /api/my/videos/:ccns as a comma separated list of CCNs
-    // Add any videos retrieved to $scope.videos
-    // var requestVideos = function(ccns) {
-    //   // $http.get('/dummy/json/videos.json').success(function(data) {
-    //   $http.get('/api/my/videos/' + ccns.join(',')).success(function(data) {
-    //     angular.extend($scope, data);
-    //     initPlayer();
-    //   });
-    // };
+      getVideos(courseDepartment, courseCategory, courseSection, courseSemester);
+    };
 
-    // Collect class page CCNs and execute request once available
-    $scope.$watchCollection('[$parent.selected_course.sections, $parent.selected_semester]', function(newValues) {
-      if (newValues[0] && newValues[1]) {
-        $scope.year = newValues[1].year,
-        $scope.semesterCode = newValues[1].code;
-        var ccns = [];
-        angular.forEach(newValues[0], function(section) {
-          ccns.push($scope.year + $scope.semesterCode + section.ccn);
-        });
+    $scope.$watch('$parent.selected_course.sections', function(newValues) {
+      if (newValues) {
         formatClassTitles();
-        // requestVideos(ccns);
       }
     });
 
