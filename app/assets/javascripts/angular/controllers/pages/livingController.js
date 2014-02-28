6d2808432ec221297884fa3cb89170ce8d94aaba
Edward Look <edwlook@gmail.com>
2014-02-28 15:38:52 -0800

    Add sort and star functionality

--- app/assets/javascripts/angular/controllers/pages/livingController.js
+++ app/assets/javascripts/angular/controllers/pages/livingController.js
@@ -20,6 +20,106 @@
       }
     ];
 
+    $scope.laundry = [
+      {
+        location: 'Unit 1 Freeborn',
+        washers: '2',
+        washersAvailable: '2',
+        washersStatus: '2/2',
+        dryers: '2',
+        dryersAvailable: '2',
+        dryersStatus: '2/2',
+        starred: false
+      },
+      {
+        location: 'Unit 1 Cheney 2',
+        washers: '2',
+        washersAvailable: '1',
+        washersStatus: '1/2',
+        dryers: '2',
+        dryersAvailable: '2',
+        dryersStatus: '2/2',
+        starred: true
+      },
+      {
+        location: 'Unit 2 Davidson 6',
+        washers: '2',
+        washersAvailable: '2',
+        washersStatus: '2/2',
+        dryers: '2',
+        dryersAvailable: '2',
+        dryersStatus: '2/2',
+        starred: false
+      },
+      {
+        location: 'Foothill Hillside',
+        washers: '10',
+        washersAvailable: '6',
+        washersStatus: '6/10',
+        dryers: '9',
+        dryersAvailable: '3',
+        dryersStatus: '3/9',
+        starred: false
+      },
+      {
+        location: 'CKC Building 11 Lower',
+        washers: '2',
+        washersAvailable: '2',
+        washersStatus: '2/2',
+        dryers: '2',
+        dryersAvailable: '2',
+        dryersStatus: '2/2',
+        starred: false
+      },
+      {
+        location: 'International House',
+        washers: '9',
+        washersAvailable: '4',
+        washersStatus: '4/9',
+        dryers: '12',
+        dryersAvailable: '10',
+        dryersStatus: '10/12',
+        starred: false
+      }
+    ];
+
+    $scope.toggleStar = function(machine) {
+      var indexOfMachine = $scope.laundry.indexOf(machine);
+      $scope.laundry[indexOfMachine].starred = !$scope.laundry[indexOfMachine].starred;
+    };
+
+    $scope.getStarredClass = function(machine) {
+      if (machine.starred) {
+        return 'starred';
+      }
+    };
+
+    //http://jsfiddle.net/vojtajina/js64b/14/
+    $scope.sort = {
+      column: ['starred', '-location'],
+      descending: true
+    };
+
+    /**
+     * Return the right sorting class for the table headers
+     */
+    $scope.getSortClass = function(column) {
+      var sortUpDown = $scope.sort.descending ? 'down' : 'up';
+      return $scope.sort.column.indexOf(column) !== -1 && 'fa fa-chevron-' + sortUpDown;
+    };
+
+    /**
+     * Change the sorting for a certain column
+     */
+    $scope.changeSorting = function(column) {
+      var sort = $scope.sort;
+      if (angular.equals(sort.column, [column])) {
+        sort.descending = !sort.descending;
+      } else {
+        sort.column = [column];
+        sort.descending = false;
+      }
+    };
 
     /**
      * Get the category name, when you feed in an id
